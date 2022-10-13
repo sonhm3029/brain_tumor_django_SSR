588a1ce96132f3a5bfdac74f62147104c5fbb896
Tuan Ngo Minh <minhtuandktd@gmail.com>
2022-10-13 11:07:13 +0700

    Add file

---
+++ BrainTumorDeployment/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', include('BrainTumorApp.urls')),
+    path('admin/', admin.site.urls),
+]
+
+if settings.DEBUG:
+    # setting this to view media files from admin panel
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
